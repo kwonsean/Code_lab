5e40ba73022f14e5ed930a9b3648fb2066a76194
kwonsean <ksh96@kw.ac.kr>
2022-03-17 17:36:19 +0900

    feat: Change JS code
    - 제이쿼리 코드를 바닐라로 수정

--- 폰트 미리보기 예제/main.js
+++ 폰트 미리보기 예제/main.js
@@ -1,63 +1,137 @@
-(function ($) {
-  var fontTxt = $('#fontTxt'); // font
-  var fontTxtField = $('#fontTxtField');
-  var fontsSelectBox = $('#fontsSelectBox');
-  var fontSizeSelectBox = $('#fontSizeSelectBox');
+const fontPreviw = document.getElementById('fontTxtField');
+const fontSizeSelectBox = document.getElementById('fontSizeSelectBox');
+const fontsSelectBox = document.getElementById('fontsSelectBox');
+const chekboxList = document.getElementsByClassName('checkbox-label');
 
-  $(fontSizeSelectBox).selectmenu({
-    change: function (event, data) {
-      fontTxtField
-        .removeClass('font-size-16')
-        .removeClass('font-size-20')
-        .removeClass('font-size-40')
-        .removeClass('font-size-72')
-        .removeClass('font-size-92')
-        .addClass(data.item.value);
-    },
-  });
+const fontList = [
+  'openSans',
+  'oswald',
+  'anton',
+  'kaushanScript',
+  'rochester',
+  'sacramento',
+];
 
-  $(fontsSelectBox).selectmenu({
-    change: function (event, data) {
-      fontTxtField
-        .removeClass('openSans')
-        .removeClass('oswald')
-        .removeClass('anton')
-        .removeClass('kaushanScript')
-        .removeClass('rochester')
-        .removeClass('sacramento')
-        .addClass(data.item.value);
-    },
-    icons: { button: 'ui-icon-circle-triangle-s' },
-  });
+const fontSizeList = [
+  'font-size-16',
+  'font-size-20',
+  'font-size-40',
+  'font-size-72',
+  'font-size-92',
+];
 
-  $('#uppercase, #lowercase, #capitalize, #normal').change(function () {
-    var value = this.value;
-    switch (value) {
-      case 'uppercase':
-        fontTxtField
-          .addClass('uppercase-text')
-          .removeClass('lowercase-text')
-          .removeClass('capitalize-text');
-        break;
-      case 'lowercase':
-        fontTxtField
-          .addClass('lowercase-text')
-          .removeClass('uppercase-text')
-          .removeClass('capitalize-text');
-        break;
-      case 'capitalize':
-        fontTxtField
-          .addClass('capitalize-text')
-          .removeClass('uppercase-text')
-          .removeClass('lowercase-text');
-        break;
-      case 'normal':
-      default:
-        fontTxtField
-          .removeClass('uppercase-text')
-          .removeClass('lowercase-text')
-          .removeClass('capitalize-text');
-        break;
-    }
-  });
-})(jQuery);
+const fontTransformList = [
+  'uppercase-text',
+  'lowercase-text',
+  'capitalize-text',
+];
+
+const handleFontChange = e => {
+  const selectedFont = e.target.value;
+  fontPreviw.classList.remove(...fontList);
+  fontPreviw.classList.add(selectedFont);
+};
+
+const handleFontSizeChange = e => {
+  const selectedFontSize = e.target.value;
+  fontPreviw.classList.remove(...fontSizeList);
+  fontPreviw.classList.add(selectedFontSize);
+};
+
+const handleFontTransform = e => {
+  const selectedFontTransform = e.target.value;
+  let selectedFontTransformClassName = 'normal';
+  fontPreviw.classList.remove(...fontTransformList);
+
+  switch (selectedFontTransform) {
+    case 'nomal':
+      break;
+    case 'uppercase':
+      selectedFontTransformClassName = 'uppercase-text';
+      break;
+    case 'lowercase':
+      selectedFontTransformClassName = 'lowercase-text';
+      break;
+    case 'capitalize':
+      selectedFontTransformClassName = 'capitalize-text';
+      break;
+    default:
+      break;
+  }
+
+  selectedFontTransformClassName === 'normal'
+    ? null
+    : fontPreviw.classList.add(selectedFontTransformClassName);
+};
+
+for (let i = 0; i < chekboxList.length; i++) {
+  chekboxList[i].addEventListener('click', handleFontTransform);
+}
+
+fontsSelectBox.addEventListener('change', handleFontChange);
+
+fontSizeSelectBox.addEventListener('change', handleFontSizeChange);
+
+// (function ($) {
+//   var fontTxt = $('#fontTxt'); // font
+//   var fontTxtField = $('#fontTxtField');
+//   var fontsSelectBox = $('#fontsSelectBox');
+//   var fontSizeSelectBox = $('#fontSizeSelectBox');
+
+//   $(fontSizeSelectBox).selectmenu({
+//     change: function (event, data) {
+//       fontTxtField
+//         .removeClass('font-size-16')
+//         .removeClass('font-size-20')
+//         .removeClass('font-size-40')
+//         .removeClass('font-size-72')
+//         .removeClass('font-size-92')
+//         .addClass(data.item.value);
+//     },
+//   });
+
+//   $(fontsSelectBox).selectmenu({
+//     change: function (event, data) {
+//       fontTxtField
+//         .removeClass('openSans')
+//         .removeClass('oswald')
+//         .removeClass('anton')
+//         .removeClass('kaushanScript')
+//         .removeClass('rochester')
+//         .removeClass('sacramento')
+//         .addClass(data.item.value);
+//     },
+//     icons: { button: 'ui-icon-circle-triangle-s' },
+//   });
+
+//   $('#uppercase, #lowercase, #capitalize, #normal').change(function () {
+//     var value = this.value;
+//     switch (value) {
+//       case 'uppercase':
+//         fontTxtField
+//           .addClass('uppercase-text')
+//           .removeClass('lowercase-text')
+//           .removeClass('capitalize-text');
+//         break;
+//       case 'lowercase':
+//         fontTxtField
+//           .addClass('lowercase-text')
+//           .removeClass('uppercase-text')
+//           .removeClass('capitalize-text');
+//         break;
+//       case 'capitalize':
+//         fontTxtField
+//           .addClass('capitalize-text')
+//           .removeClass('uppercase-text')
+//           .removeClass('lowercase-text');
+//         break;
+//       case 'normal':
+//       default:
+//         fontTxtField
+//           .removeClass('uppercase-text')
+//           .removeClass('lowercase-text')
+//           .removeClass('capitalize-text');
+//         break;
+//     }
+//   });
+// })(jQuery);
